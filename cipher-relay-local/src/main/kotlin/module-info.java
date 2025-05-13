817188220fa55f2ca6f860625fb11055480282ac
yag <marks.yag@gmail.com>
2025-05-14 00:43:06 +0800

    :arrow_up: Upgrade netty to 4.2.1.

--- cipher-relay-local/src/main/kotlin/module-info.java
+++ cipher-relay-local/src/main/kotlin/module-info.java
@@ -1,6 +1,13 @@
 module com.github.yag.cr.local {
     requires kotlin.stdlib;
     requires org.slf4j;
+    requires io.netty.buffer;
+    requires io.netty.codec;
+    requires io.netty.codec.http;
+    requires io.netty.codec.socks;
+    requires io.netty.common;
+    requires io.netty.handler;
+    requires io.netty.transport;
     
     requires com.github.yag.cr.protocol;
     
--- cipher-relay-protocol/src/main/kotlin/module-info.java
+++ cipher-relay-protocol/src/main/kotlin/module-info.java
@@ -2,6 +2,8 @@ module com.github.yag.cr.protocol {
     requires kotlin.stdlib;
     requires org.slf4j;
     requires ketty.core;
+    requires io.netty.buffer;
+    requires io.netty.common;
     
     exports com.github.yag.cr.protocol;
 }
